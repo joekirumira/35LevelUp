da74ba92fd46e8af0448348b11fdcb1347c07b2f
Kirumira <joekirumira3@gmail.com>
2018-12-14 15:49:09 +0300

    fix:edits for amount_in_bank

--- amount_in_bank.py
+++ amount_in_bank.py
@@ -2,17 +2,19 @@ def balance_in_bank():
     amount_in_bank = 500
     new_amount_in_bank = 500+0
     transact = input("Enter key and value separated by commas (,): ")
-    key, value = transact.split(",")
-    for key in transact:
-        if key == D or key == d:
-            amount_in_bank += transact[key]
-            amount_in_bank = new_amount_in_bank
-        elif key == W or key == w:
-            amount_in_bank -= transact[key]
-            amount_in_bank = new_amount_in_bank
-        else:
-            if amount_in_bank <= 0:
-                print("You have reached your minimum balance")
+    transaction, amount = transact.split(",")
+    # print(key)
+    # print(value)
+    amount = int(amount)
+    if transaction.lower() == "d":
+        amount_in_bank += amount
+        new_amount_in_bank = amount_in_bank
+    elif transaction.lower() == "w":
+        amount_in_bank -= amount
+        new_amount_in_bank = amount_in_bank
+    else:
+        if amount_in_bank <= 0:
+            print("You have reached your minimum balance")
 
     return new_amount_in_bank
 
